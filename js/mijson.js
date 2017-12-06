0d3bf41d46c241558f2e630f3f438d62fb74bb63
Jesus David Gonzalez Pascolo <34315460+jesdagonpasdev@users.noreply.github.com>
2017-12-06 19:18:32 +0000

    Create mijson.js

---
+++ js/mijson.js
@@ -0,0 +1,293 @@
+var contenido = [{    
+        "tipo": "Grado Medio",
+        "ciclo": "Sistemas Microinformáticos y Redes",
+        "pais": "Italia",
+        "ciudad": "Barcellona Pozzo di Gotto"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Sistemas Microinformáticos y Redes",
+        "pais": "Italia",
+        "ciudad": "Vicenza"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Sistemas Microinformáticos y Redes",
+        "pais": "Italia",
+        "ciudad": "Brescia"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Sistemas Microinformáticos y Redes",
+        "pais": "Polonia",
+        "ciudad": "Cracovia"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Sistemas Microinformáticos y Redes",
+        "pais": "Reino Unido",
+        "ciudad": "Bournemonth"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Sistemas Microinformáticos y Redes",
+        "pais": "República Checa",
+        "ciudad": "Praga"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Instalaciones de Telecomunicaciones",
+        "pais": "Italia",
+        "ciudad": "Vicenza"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Instalaciones de Telecomunicaciones",
+        "pais": "Italia",
+        "ciudad": "Barcellona Pozzo di Gotto"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Instalaciones Eléctricas y Automatismos",
+        "pais": "Italia",
+        "ciudad": "Barcellona Pozzo di Gotto"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Instalaciones Eléctricas y Automatismos",
+        "pais": "Italia",
+        "ciudad": "Catania"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Instalaciones Eléctricas y Automatismos",
+        "pais": "República Checa",
+        "ciudad": "Praga"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Gestión Administrativa",
+        "pais": "Italia",
+        "ciudad": "Barcellona Pozzo di Gotto"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Gestión Comercial",
+        "pais": "Italia",
+        "ciudad": "Barcellona Pozzo di Gotto"
+      },{
+        "tipo": "Grado Medio",
+        "ciclo": "Gestión Comercial",
+        "pais": "República Checa",
+        "ciudad": "Praga"
+      },{
+        "tipo": "Profesorado",
+        "ciclo": "Informática y Comunicaciones",
+        "pais": "Italia",
+        "ciudad": "Brescia"
+      },{
+        "tipo": "Profesorado",
+        "ciclo": "Informática y Comunicaciones",
+        "pais": "Italia",
+        "ciudad": "Barcellona Pozzo di Gotto"
+      },{
+        "tipo": "Profesorado",
+        "ciclo": "Informática y Comunicaciones",
+        "pais": "Italia",
+        "ciudad": "Bologna"
+      },{
+        "tipo": "Profesorado",
+        "ciclo": "Imagen y Sonido",
+        "pais": "Reino Unido",
+        "ciudad": "Londres"
+      },{
+        "tipo": "Profesorado",
+        "ciclo": "Comercio y Márketing",
+        "pais": "Reino Unido",
+        "ciudad": "Bournemonth",
+        "": {}
+      },{
+        "tipo": "Profesorado",
+        "ciclo": "Comercio y Márketing",
+        "pais": "Reino Unido",
+        "ciudad": "Londres"
+      },{
+        "tipo": "Profesorado",
+        "ciclo": "Electricidad y Electronica",
+        "pais": "Polonia",
+        "ciudad": "Wroclaw",
+        "": {}
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración y Finanzas",
+        "pais": "Reino Unido",
+        "ciudad": "Oxford"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Desarrollo de Aplicaciones Web",
+        "pais": "República Checa",
+        "ciudad": "Praga"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración de Sistemas Informáticos en Red",
+        "pais": "Irlanda",
+        "ciudad": "Cork"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Realización de Audiovisuales y Espectáculos",
+        "pais": "Italia",
+        "ciudad": "Tolentino"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Realización de Audiovisuales y Espectáculos",
+        "pais": "Alemania",
+        "ciudad": "Berlin"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Comercio Internacional",
+        "pais": "Amsterdam",
+        "ciudad": "Holanda"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración de Sistemas Informáticos en Red",
+        "pais": "Polonia",
+        "ciudad": "Cracovia"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración de Sistemas Informáticos en Red",
+        "pais": "Malta",
+        "ciudad": "Qormi"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Realización de Audiovisuales y Espectáculos",
+        "pais": "Reino Unido",
+        "ciudad": "Plymouth"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración de Sistemas Informáticos en Red",
+        "pais": "Malta",
+        "ciudad": "Attard"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Mantenimiento Electrónico",
+        "pais": "Reino Unido",
+        "ciudad": "Londres"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Realización de Audiovisuales y Espectáculos",
+        "pais": "Reino Unido",
+        "ciudad": "Londres"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Desarrollo de Aplicaciones Multiplataforma",
+        "pais": "Irlanda",
+        "ciudad": "Dublin"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración y Finanzas",
+        "pais": "Malta",
+        "ciudad": "La Valletta"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Realización de Audiovisuales y Espectáculos",
+        "pais": "Irlanda",
+        "ciudad": "Ballinasloe"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Producción de Audiovisuales y Espectáculos",
+        "pais": "Irlanda",
+        "ciudad": "Carlow"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Marketing y Publicidad",
+        "pais": "Hungría",
+        "ciudad": "Budapest"
+    },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración de Sistemas Informáticos en Red",
+        "pais": "Irlanda",
+        "ciudad": "Dublin"
+    },{
+        "tipo": "Grado Superior",
+        "ciclo": "Desarrollo de Aplicaciones Web",
+        "pais": "República Checa",
+        "ciudad": "Praga"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Comercio Internacional",
+        "pais": "Irlanda",
+        "ciudad": "Cork"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Gestión de Ventas y Espacios Comerciales",
+        "pais": "Irlanda",
+        "ciudad": "Cork"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Desarrollo de Aplicaciones Web",
+        "pais": "Irlanda",
+        "ciudad": "Cork"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración y Finanzas",
+        "pais": "Irlanda",
+        "ciudad": "Cork"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Comercio y Marketing",
+        "pais": "Reino Unido",
+        "ciudad": "Bournemonth"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Marketing y Publicidad",
+        "pais": "Reino Unido",
+        "ciudad": "Londres"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Comercio y Marketing",
+        "pais": "Reino Unido",
+        "ciudad": "Londres"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Realización de Audiovisuales y Espectáculos",
+        "pais": "Reino Unido",
+        "ciudad": "Brighton"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Transporte y Logística",
+        "pais": "Malta",
+        "ciudad": "Iklin"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración y Finanzas",
+        "pais": "Noruega",
+        "ciudad": "Sortland"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración de Sistemas Informáticos y Redes",
+        "pais": "República Checa",
+        "ciudad": "Praga"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración de Sistemas Informáticos y Redes",
+        "pais": "Polonia",
+        "ciudad": "Cracovia"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Realización de Audiovisuales y Espectáculos",
+        "pais": "Polonia",
+        "ciudad": "Cracovia"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Adminstración y Finanzas",
+        "pais": "Polonia",
+        "ciudad": "Cracovia"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Administración de Sistemas Informáticos y Redes",
+        "pais": "Reino Unido",
+        "ciudad": "Brighton"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Comercio Internacional",
+        "pais": "República Checa",
+        "ciudad": "Praga"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Comercio Internacional",
+        "pais": "Malta",
+        "ciudad": "La Valletta"
+      },{
+        "tipo": "Grado Superior",
+        "ciclo": "Producción de Audiovisuales y Espectáculos",
+        "pais": "Reino Unido",
+        "ciudad": "Brighton"
+    }];
